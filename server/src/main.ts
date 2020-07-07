f1d22032c07b7d48e7985d6d309e743e33f0501e
Hubert <hwawrzacz.dev@gmail.com>
2020-07-07 18:59:06 +0200

    Initialize server

---
+++ server/src/main.ts
@@ -0,0 +1,7 @@
+export class Server {
+    constructor() {
+        console.log('Server is up!');
+    }
+}
+
+const server = new Server();
---
+++ server/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/main.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'server.js'
+    },
+    watch: true
+}
